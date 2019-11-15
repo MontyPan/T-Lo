23df11a4d336ad1b6354bf8865ae9b858d877cf5
MontyPan <monty.pan@dontcareabout.us>
2019-11-15 22:13:23 +0800

    增加：迴圈地獄起點

---
+++ basic/src/main/java/lo/basic/TaskLoop.java
@@ -0,0 +1,16 @@
+package lo.basic;
+
+public class TaskLoop {
+	public static void main(String[] args) {
+		int input = 5;
+
+		System.out.println("start");	//可以砍掉
+
+		for (int i = 0; i < input; i++) {
+			System.out.print("*****");	//不會換行
+			System.out.println();	//換行
+		}
+
+		System.out.println("end");	//可以砍掉
+	}
+}
