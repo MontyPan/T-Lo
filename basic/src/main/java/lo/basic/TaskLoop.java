5e6ea28e16fe9ef6366c0fa89b4526bad86c0ab8
Gloomy <52154705+G100my@users.noreply.github.com>
2019-11-17 16:46:33 +0800

    刪除 不必要的 comment

--- basic/src/main/java/lo/basic/TaskLoop.java
+++ basic/src/main/java/lo/basic/TaskLoop.java
@@ -4,15 +4,15 @@ public class TaskLoop {
 	public static void main(String[] args) {
 		int input = 15;
 
-		System.out.println("start");	//可以砍掉
+		System.out.println("start");	
 
 		for (int i = 0; i < input; i++) {
 			for (int ii = 0;ii < input; ii++) {
-				System.out.print("*" );	//不會換行
+				System.out.print("*" );	
 			}
-			System.out.println();	//換行
+			System.out.println();	
 		}
 
-		System.out.println("end");	//可以砍掉
+		System.out.println("end");	
 	}
 }
