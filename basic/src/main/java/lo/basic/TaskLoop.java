858ee5084d6e088d01367f10ff1b6e48d050ec8f
G100my <G100my.lan229@gmail.com>
2019-11-17 13:50:15 +0800

    迴圈地獄第一層 #5 增加巢狀 forloop

--- basic/src/main/java/lo/basic/TaskLoop.java
+++ basic/src/main/java/lo/basic/TaskLoop.java
@@ -2,12 +2,14 @@ package lo.basic;
 
 public class TaskLoop {
 	public static void main(String[] args) {
-		int input = 5;
+		int input = 15;
 
 		System.out.println("start");	//可以砍掉
 
 		for (int i = 0; i < input; i++) {
-			System.out.print("*****");	//不會換行
+			for (int ii = 0;ii < input; ii++) {
+			System.out.print("*" );	//不會換行
+			}
 			System.out.println();	//換行
 		}
 
