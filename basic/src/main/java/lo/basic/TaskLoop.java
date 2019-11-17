f188203ee242612d0f7bdaf05e619a7cdf9ecab5
MontyPan <monty.pan@dontcareabout.us>
2019-11-17 17:24:12 +0800

    extract task5()

--- basic/src/main/java/lo/basic/TaskLoop.java
+++ basic/src/main/java/lo/basic/TaskLoop.java
@@ -2,17 +2,19 @@ package lo.basic;
 
 public class TaskLoop {
 	public static void main(String[] args) {
-		int input = 15;
+		issue5(15);	//Delete
+	}
 
-		System.out.println("start");	
+	private static void issue5(int input) {
+		System.out.println("start");
 
 		for (int i = 0; i < input; i++) {
 			for (int ii = 0;ii < input; ii++) {
-				System.out.print("*" );	
+				System.out.print("*" );
 			}
-			System.out.println();	
+			System.out.println();
 		}
 
-		System.out.println("end");	
+		System.out.println("end");
 	}
 }
