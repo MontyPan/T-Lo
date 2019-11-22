56b7411d4c9c86d70340dffe440d93b072f0cf13
G100my <G100my.lan229@gmail.com>
2019-11-22 17:14:34 +0800

    完成 issue7

--- basic/src/main/java/lo/basic/TaskLoop.java
+++ basic/src/main/java/lo/basic/TaskLoop.java
@@ -2,19 +2,60 @@ package lo.basic;
 
 public class TaskLoop {
 	public static void main(String[] args) {
-		issue5(15);	//Delete
+		issue7_1(3);
+		issue7_2(3);
+		issue7_3(3);
 	}
 
-	private static void issue5(int input) {
-		System.out.println("start");
-
-		for (int i = 0; i < input; i++) {
-			for (int ii = 0;ii < input; ii++) {
-				System.out.print("*" );
+	private static void issue7_1(int input_1) {
+		System.out.println("start_1");
+		for (int i = 0; i < input_1; i++) {
+			for (int ii = 0; ii <= i; ii++) {
+				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println("");
 		}
+		for (int i = 0; i < input_1 - 1; i++) {
+			for (int ii = 1; ii < input_1 - i; ii++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("end_1");
+	}
 
-		System.out.println("end");
+	private static void issue7_2(int input_2) {
+		System.out.println("start_2");
+		for (int i = input_2; i > 0; i--) {
+			for (int ii = input_2 - i; ii >= 0; ii--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		for (int i = input_2; i > 1; i--) {
+			for (int ii = i - 1; ii >= 1; ii--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("end_2");
+	}
+
+	private static void issue7_3(int input_3) {
+		System.out.println("start_3");
+		int[] ls = new int[input_3 * 2 - 1];
+		for (int i = 0; i < input_3; i++) {
+			ls[i] = i + 1;
+		}
+		for (int i = input_3; i < input_3 * 2 - 1; i++) {
+			ls[i] = input_3 * 2 - i - 1;
+		}
+		for (int i = 0; i < input_3 * 2 - 1; i++) {
+			for (int ii = 0; ii < ls[i]; ii++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		System.out.println("end_3");
 	}
 }
