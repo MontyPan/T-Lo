197f0dfdb740cd6334fe7fb1a6981c2e0b5fad2a
G100my <G100my.lan229@gmail.com>
2019-11-26 19:07:15 +0800

    新增 issue7_4 訂正 Must Fix
    Must Fix: 補回 issue5、更改變數

--- basic/src/main/java/lo/basic/TaskLoop.java
+++ basic/src/main/java/lo/basic/TaskLoop.java
@@ -2,21 +2,36 @@ package lo.basic;
 
 public class TaskLoop {
 	public static void main(String[] args) {
-		issue7_1(3);
-		issue7_2(3);
-		issue7_3(3);
+
+		int totalInput = 2;
+		issue5(totalInput);
+		issue7_1(totalInput);
+		issue7_2(totalInput);
+		issue7_3(totalInput);
+		issue7_4(totalInput);
 	}
 
-	private static void issue7_1(int input_1) {
+	private static void issue5(int input) {
+		System.out.println("start");
+		for (int i = 0; i < input; i++) {
+			for (int ii = 0; ii < input; ii++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("end");
+	}
+
+	private static void issue7_1(int input) {
 		System.out.println("start_1");
-		for (int i = 0; i < input_1; i++) {
+		for (int i = 0; i < input; i++) {
 			for (int ii = 0; ii <= i; ii++) {
 				System.out.print("*");
 			}
 			System.out.println("");
 		}
-		for (int i = 0; i < input_1 - 1; i++) {
-			for (int ii = 1; ii < input_1 - i; ii++) {
+		for (int i = 0; i < input - 1; i++) {
+			for (int ii = 1; ii < input - i; ii++) {
 				System.out.print("*");
 			}
 			System.out.println("");
@@ -24,15 +39,15 @@ public class TaskLoop {
 		System.out.println("end_1");
 	}
 
-	private static void issue7_2(int input_2) {
+	private static void issue7_2(int input) {
 		System.out.println("start_2");
-		for (int i = input_2; i > 0; i--) {
-			for (int ii = input_2 - i; ii >= 0; ii--) {
+		for (int i = input; i > 0; i--) {
+			for (int ii = input - i; ii >= 0; ii--) {
 				System.out.print("*");
 			}
 			System.out.println("");
 		}
-		for (int i = input_2; i > 1; i--) {
+		for (int i = input; i > 1; i--) {
 			for (int ii = i - 1; ii >= 1; ii--) {
 				System.out.print("*");
 			}
@@ -41,21 +56,34 @@ public class TaskLoop {
 		System.out.println("end_2");
 	}
 
-	private static void issue7_3(int input_3) {
+	private static void issue7_3(int input) {
 		System.out.println("start_3");
-		int[] ls = new int[input_3 * 2 - 1];
-		for (int i = 0; i < input_3; i++) {
-			ls[i] = i + 1;
+		int[] list = new int[input * 2 - 1];
+		for (int i = 0; i < input; i++) {
+			list[i] = i + 1;
 		}
-		for (int i = input_3; i < input_3 * 2 - 1; i++) {
-			ls[i] = input_3 * 2 - i - 1;
+		for (int i = input; i < list.length; i++) {
+			list[i] = input * 2 - i - 1;
 		}
-		for (int i = 0; i < input_3 * 2 - 1; i++) {
-			for (int ii = 0; ii < ls[i]; ii++) {
+		for (int i = 0; i < list.length; i++) {
+			for (int ii = 0; ii < list[i]; ii++) {
 				System.out.print("*");
 			}
 			System.out.println("");
 		}
 		System.out.println("end_3");
+	}
+	private static void issue7_4(int input) {
+		System.out.println("start_4");
+		for (int i = 1; i < input * 2; i++) {
+		    for (int ii = 0; ii < i && i <= input; ii++) {
+		        System.out.print("*");
+		    }
+		    for (int ii = input * 2 - i; ii > 0 && i > input; ii--) {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+		System.out.println("end_4");
 	}
 }
