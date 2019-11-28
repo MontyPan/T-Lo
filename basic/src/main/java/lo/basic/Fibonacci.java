4a5eb74f1f5fa934f101dfee8bcf3154529c79d3
G100my <G100my.lan229@gmail.com>
2019-11-28 13:45:30 +0800

    完成 issue10

--- basic/src/main/java/lo/basic/Fibonacci.java
+++ basic/src/main/java/lo/basic/Fibonacci.java
@@ -5,21 +5,29 @@ public class Fibonacci {
 		test(0, 1, 10, 55);
 		test(1, 2, 8, 55);
 		test(1, 5, 20, 38006);
+		test(100, 101, 1, 100);
+		test(88, 99, 2, 99);
 	}
 
 	/**
-	 * a[n] = a[n-1] + a[n-1]
-	 *
+	 * a[n] = a[n-1] + a[n-1] → 應該是 a[n] = a[n-1] + a[n-2] ?
+	 * 
 	 * @return 第 n 項的值
 	 */
+	
 	private static int forLoop(int a0, int a1, int n) {
-		//假設 n 必定大於 2
-		//當然你要參考下面 test() 把他補強至 n 必定大於 0 也很好
-
 		int result = 0;
-
-		//FIXME
-
+		if (n > 2) {
+			for (int i = 1; i < n; i++) {
+				result = a0 + a1;
+				a0 = a1;
+				a1 = result;
+			}
+		} else if (n == 2) {
+			result = a1;
+		} else if (n == 1) {
+			result = a0;
+		}
 		return result;
 	}
 
