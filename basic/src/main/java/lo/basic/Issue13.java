a962fb25d6e43c09da986cb39fab5bc22dbefee8
G100my <G100my.lan229@gmail.com>
2019-12-08 07:48:08 +0800

    完成 Issue13

--- basic/src/main/java/lo/basic/Issue13.java
+++ basic/src/main/java/lo/basic/Issue13.java
@@ -4,17 +4,26 @@ import java.util.Arrays;
 
 public class Issue13 {
 	public static void main(String[] args) {
-		int[] data = {5, 5, 6, 6, 78, 78, 78, 9487, 9487, 9487, 9487, 65535};
+		int[] data = { 5, 5, 6, 6, 78, 78, 78, 9487, 9487, 9487, 9487, 65535 };
 		int[] result = longest(data);
 		System.out.println(Arrays.toString(result));
 	}
 
-	/**
-	 * @param data 已經排序過的 array
-	 * @return result[0] 是最長平台的數字，result[1] 是長度
-	 */
 	private static int[] longest(int[] data) {
-		//FIXME
-		return new int[]{9487, 4};
+		int counter = 1;
+		int max_counter = 0;
+		int number = data[0];
+		for (int i = 0, ii = 1; ii < data.length; i++, ii++) {
+			if (data[i] == data[ii]) {
+				counter += 1;
+				if (counter > max_counter) {
+					max_counter = counter;
+					number = data[i];
+				}
+			} else {
+				counter = 1;
+			}
+		}
+		return new int[] { number, max_counter };
 	}
 }
