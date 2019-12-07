dbf4264e47fc7393dae81588d448e9640a1d42c9
G100my <G100my.lan229@gmail.com>
2019-12-08 06:33:53 +0800

    第二層變數統一改為i2、新增 v4、間隔空行移至上方

--- basic/src/main/java/lo/basic/Issue12.java
+++ basic/src/main/java/lo/basic/Issue12.java
@@ -5,8 +5,12 @@ public class Issue12 {
 	public static void main(String[] args) {
 		int input = 5;
 		v1(input);
+		System.out.println();
 		v2(input);
+		System.out.println();
 		v3(input);
+		System.out.println();
+		v4(input);
 	}
 
 	public static void v1(int x) {
@@ -16,7 +20,7 @@ public class Issue12 {
 			for (int i2 = i + 1; i2 < x; i2++) {
 				System.out.print(" ");
 			}
-			for (int i3 = 0; i3 <= i * 2; i3++) {
+			for (int i2 = 0; i2 <= i * 2; i2++) {
 				if (counter < 9) {
 					System.out.print(counter);
 					counter += 1;
@@ -31,11 +35,11 @@ public class Issue12 {
 	}
 
 	public static void v2(int x) {
-		System.out.println("\nv2");
+		System.out.println("v2");
 		int counter = 1;
 		for (int i = 0; i < x; i++) {
-			for (int ii = 0; ii < x * 2 - 1; ii++) {
-				if (ii < x - i - 1 || ii > x + i - 1) {
+			for (int i2 = 0; i2 < x * 2 - 1; i2++) {
+				if (i2 < x - i - 1 || i2 > x + i - 1) {
 					System.out.print(" ");
 				} else {
 					if (counter < 9) {
@@ -52,7 +56,7 @@ public class Issue12 {
 	}
 
 	public static void v3(int x) {
-		System.out.println("\nv3");
+		System.out.println("v3");
 		int[] list = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		int index = 1;
 		int limit = x * 2;
@@ -60,8 +64,28 @@ public class Issue12 {
 			for (int i2 = 1; i2 < limit - i; i2 += 2) {
 				System.out.print(" ");
 			}
-			for (int i4 = 0; i4 < i; i4++) {
+			for (int i2 = 0; i2 < i; i2++) {
 				if (index > 9) {
+					index = 0;
+				}
+				System.out.print(list[index]);
+				index += 1;
+			}
+			System.out.println();
+		}
+	}
+
+	public static void v4(int x) {
+		System.out.println("v4");
+		int[] list = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int index = 1;
+		int limit = x * 2;
+		for (int i = 1; i < limit; i += 2) {
+			for (int i2 = 1; i2 < limit - i; i2 += 2) {
+				System.out.print(" ");
+			}
+			for (int i2 = 0; i2 < i; i2++) {
+				if (index % 10 == 0) {
 					index = 0;
 				}
 				System.out.print(list[index]);
