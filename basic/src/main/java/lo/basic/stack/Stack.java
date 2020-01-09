adf27af56794f0e0bf15741004d84ac15abf3ae9
G100my <G100my.lan229@gmail.com>
2020-01-10 03:28:20 +0800

    修改至可運作

--- basic/src/main/java/lo/basic/stack/Stack.java
+++ basic/src/main/java/lo/basic/stack/Stack.java
@@ -2,4 +2,51 @@ package lo.basic.stack;
 
 public class Stack {
 
+	int index = 0;
+	int[] stack;
+
+	public Stack() {
+		stack = new int[10];
+	}
+
+	public Stack(int[] args) {
+		stack = args;
+	}
+
+	public void push(int num) {
+		stack[index] = num;
+		index += 1;
+		if (index >= stack.length) {
+			addLength();
+		}
+	}
+
+	public int pop() {
+		index -= 1;
+		if (index < 0) {
+			lastOne();
+			index = 0;
+		}
+		return stack[index];
+	}
+
+	private void addLength() {
+		int[] tmp = stack;
+		stack = new int[index * 2];
+		for (int i = 0; i < tmp.length; i++) {
+			stack[i] = tmp[i];
+		}
+	}
+
+	private void lastOne() {
+		System.out.println("It's last one!");
+	}
+
+	public void showLength() {
+		System.out.println(stack.length);
+	}
+
+	public void whatIsNext() {
+		System.out.println(stack[index]);
+	}
 }
