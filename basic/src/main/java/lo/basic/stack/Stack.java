baa1de2af1d786975912fe5f9640ccb7ad564b69
G100my <G100my.lan229@gmail.com>
2020-01-10 14:23:41 +0800

    新增 JavaDoc

--- basic/src/main/java/lo/basic/stack/Stack.java
+++ basic/src/main/java/lo/basic/stack/Stack.java
@@ -1,9 +1,17 @@
 package lo.basic.stack;
 
+/**
+ * 跟 #16 有什麼差異：
+ * 1. 從 StackCaller 的角度看，無法知道各 method 裡面寫了什麼。
+ * 2. caller 能用 Stack __ = new Stack(); 建立實際上不一樣但是功能一樣的 Stack，各自不相干擾的 Stack。
+ * 3. caller 的介面(？　欸...不知道怎麼稱呼= =+)（StackCaller.java)）看起來比較簡潔，
+ *    沒有一長串定義 method 的程式碼。
+ * 4. 能限定存取權限，例如 field 開成 private 可以避免 值（例如：index） 從該 class 以外的地方直接被改掉。
+ */
 public class Stack {
 
-	int index = 0;
-	int[] stack;
+	private int index = 0;
+	private int[] stack;
 
 	public Stack() {
 		stack = new int[10];
@@ -21,6 +29,14 @@ public class Stack {
 		}
 	}
 
+	/**
+	 * @return 回傳由 index 指向 stack[] 內第幾個位置的值
+	 * @exception 若 index = 0 仍繼續執行 pop()，將列印警示、將 index = 0 並傳回 stack[index]。
+	 *
+	 * 因為 pop() 比 push() 多了「可能是 caller 非預期的例外狀況」，
+	 * 例如 1. 列印出警示  2. index < 0 的情況下會傳回 stack[0]的值，
+	 * 故需要特別說明。
+	 */
 	public int pop() {
 		index -= 1;
 		if (index < 0) {
@@ -46,7 +62,11 @@ public class Stack {
 		System.out.println(stack.length);
 	}
 
+	/**
+	 * A:可以先查看下一個 pop 的值 / 剛剛不小心 push 進去的值是什麼。
+	 * (少打了 -1  Q_Q)
+	 */
 	public void whatIsNext() {
-		System.out.println(stack[index]);
+		System.out.println(stack[index-1]);
 	}
 }
