df06608ccfdafe467f9fc2931a05903ff3255519
G100my <G100my.lan229@gmail.com>
2020-01-18 16:24:35 +0800

    新增 縮短長度

--- basic/src/main/java/lo/basic/stack/Stack.java
+++ basic/src/main/java/lo/basic/stack/Stack.java
@@ -1,14 +1,13 @@
 package lo.basic.stack;
 
-import java.util.Arrays;
-
 /**
  * 跟 #16 有什麼差異：
  * <ol>
- * <li>從 StackCaller 的角度看，無法知道各 method 裡面寫了什麼。</li>
- * <li>caller 能用 Stack __ = new Stack(); 建立實際上不一樣但是功能一樣的 Stack，各自不相干擾的Stack。</li>
- * <li>caller 的介面(？ 欸...不知道怎麼稱呼= =+)（StackCaller.java)）看起來比較簡潔，<br>沒有一長串定義 method 的程式碼。</li>
- * <li>能限定存取權限，例如 field 開成 private 可以避免 值（例如：index） 從該 class 以外的地方直接被改掉。</li>
+ * 	<li>從 StackCaller 的角度看，無法知道各 method 裡面寫了什麼。</li>
+ * 	<li>caller 能用 Stack __ = new Stack(); 建立實際上不一樣但是功能一樣的Stack，各自不相干擾的Stack。</li>
+ * 	<li>caller 的介面(？ 欸...不知道怎麼稱呼= =+)（StackCaller.java)）看起來比較簡潔，<br>
+ * 		沒有一長串定義 method 的程式碼。</li>
+ * 	<li>能限定存取權限，例如 field 開成 private 可以避免 值（例如：index） 從該 class 以外的地方直接被改掉。</li>
  * </ol>
  */
 public class Stack {
@@ -23,7 +22,6 @@ public class Stack {
 	public Stack(int[] args) {
 		stack = args;
 		index = stack.length;
-
 	}
 
 	public void push(int num) {
@@ -37,18 +35,24 @@ public class Stack {
 	/**
 	 * 因為 pop() 比 push() 多了「可能是 caller 非預期的例外狀況」，故需要特別說明。<br>
 	 * 例如
-	 * <ol><li>列印出警示</li>
-	 * <li>index < 0 的情況下會傳回 stack[0]的值。</li></ol>
-	 *
-	 * 若 index = 0 仍繼續執行 pop()，將列印警示、將 index = 0 並傳回 stack[index]。
-	 *
-	 * @return 由 index 指向 stack[] 內第幾個位置的值。
+	 * <ol>
+	 * 	<li>列印出警示</li>
+	 * 	<li>index < 0 的情況下會傳回 stack[0]的值。</li>
+	 * </ol>
+	 * <ol>
+	 * 	<li>若 index = 0 仍繼續執行 pop()，將列印警示、將 index = 0 並傳回 stack[index]。</li>
+	 * 	<li>若 index < stack.length / 3、且 stack 的長度 > 長度初始值 10，將呼叫 minusLength()，使 stack 長度減半。</li>
+	 * </ol>
+	 * @return 由 index 指向s stack[] 內第幾個位置的值。
 	 */
 	public int pop() {
 		index -= 1;
 		if (index < 0) {
 			lastOne();
 			index = 0;
+		}
+		if (index < stack.length / 3 && stack.length > 10) {
+			minusLength();
 		}
 		return stack[index];
 	}
@@ -57,6 +61,17 @@ public class Stack {
 		int[] tmp = stack;
 		stack = new int[index * 2];
 		for (int i = 0; i < tmp.length; i++) {
+			stack[i] = tmp[i];
+		}
+	}
+	
+	/**
+	 * 從 tmp 複製到 stack(new) 的內容僅到 index - 1 所指的地方
+	 */
+	private void minusLength() {
+		int[] tmp = stack;
+		stack = new int[tmp.length / 2];
+		for (int i = 0; i < index; i++) {
 			stack[i] = tmp[i];
 		}
 	}
