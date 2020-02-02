8bc397ce341cda7ce6daacf5d243dfaafe2a3b2e
G100my <G100my.lan229@gmail.com>
2020-02-02 22:14:03 +0800

    增加 caller 測試數字

--- basic/src/main/java/lo/basic/abb/AbbCaller.java
+++ basic/src/main/java/lo/basic/abb/AbbCaller.java
@@ -8,11 +8,12 @@ public class AbbCaller {
 		Abb test = new Abb();
 		
 		double num1 = 234632346;
-		double num2 = 1312312312312312.1241454353d;
+		double num2 = 23123123145343636346454555555555555555555555555555555555555555555555555552312312.1241454352342342235355658769876543567654565434563d;
 		double num3 = 0.23562d;
 		double num4 = 1000;
 		double num5 = -1000;
 		double num6 = -999.99d;
+		double num7 = -13463654747655686585.353455464765878556534114325436544767d;
 		
 		System.out.println(test.main(num1));
 		System.out.println(test.main(num2));
@@ -20,5 +21,6 @@ public class AbbCaller {
 		System.out.println(test.main(num4));
 		System.out.println(test.main(num5));
 		System.out.println(test.main(num6));
+		System.out.println(test.main(num7));
 	}
 }
